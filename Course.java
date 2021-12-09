6cd78a4ea1907480c925c9dcc30356e43ced6410
SamDG7 <89500055+SamDG7@users.noreply.github.com>
2021-12-09 16:31:58 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,6 +1,9 @@
-import javax.swing.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Course
@@ -16,69 +19,181 @@ import java.util.ArrayList;
  */
 
 public class Course {
-    public static String[] quizzes;
-    public static Object quizName;
-    public static String[] courses;
-    public static Object courseName;
+    /**
+     * The name of the course.
+     */
+    public String courseName;
 
-    public void getCourses() throws FileNotFoundException, IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader("Courses.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.contains(courseName)) {
-                    line = line.substring(line.indexOf(' ') + 1);
-                    line = line.substring((line.indexOf(' ') + 1));
-                    line = line.substring(0, line.indexOf(' '));
-                    courses[Integer.parseInt(line)] = line + ", ";
-                }
-            }
-            int lastCourse = courses.length + 1;
-            courses[lastCourse] = "Exit";
-            courseName = JOptionPane.showInputDialog(null,
-                    "Please select a course to access.", "Brightspace",
-                    JOptionPane.INFORMATION_MESSAGE, null,
-                    courses, courses[lastCourse]);
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * The ArrayList that stores the names of the existing courses.
+     */
+    public ArrayList<String> courses;
+
+    /**
+     * The number of courses.
+     */
+    public int courseNumber;
+
+    /**
+     * Constructs a newly allocated object with the name of the course.
+     * @param courseName
+     */
+    public Course(String courseName) {
+        this.courseName = courseName;
+        courses = new ArrayList<String>();
+    }
+
+    /**
+     * Method only for teachers to create a new course
+     */
+    public void createCourse() {
+        /**
+         * Scanner
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the name of the new course:");
+        /**
+         * Input value for the name of the course
+         */
+        courseName = scanner.nextLine();
+
+        /**
+         * The name of the new course is added to the ArrayList of courses
+         */
+        courses.add(courseName);
+        System.out.println("Course successfully added!");
+    }
+
+    /**
+     * Method only for teachers
+     * Edits existing course
+     */
+    public void editCourse() {
+        Scanner scanner = new Scanner(System.in);
+        /**
+         * Accesses list of courses
+         */
+        getCourses();
+        System.out.println("Select the number of the course to edit:");
+        /**
+         * Input value for the number of the course to be edited
+         */
+        courseNumber = scanner.nextInt();
+        System.out.println("Please enter the new name for the course:");
+        /**
+         * Input value for the new name of the course
+         */
+        courseName = scanner.nextLine();
+        /**
+         * Changes name in the ArrayList
+         */
+        courses.set(courseNumber - 1, courseName);
+        System.out.println("Course successfully edited!");
+    }
+
+    /**
+     * Method only for teachers
+     * Deletes course
+     */
+    public void deleteCourse() {
+        Scanner scanner = new Scanner(System.in);
+        /**
+         * Accesses the list of the courses
+         */
+        getCourses();
+        System.out.println("Please select the number of the course to remove:");
+        /**
+         * Input value for the number of the course to be deleted
+         */
+        courseNumber = scanner.nextInt();
+        /**
+         * Accesses the index number of the course and deleted entry from the ArrayList of courses
+         */
+        courses.remove(courseNumber - 1);
+        System.out.println("Course successfully removed!");
+    }
+
+    /**
+     * Creates an ArrayList of courses
+     * @return
+     */
+    public ArrayList<String> courses() {
+        for (int i = 0; i < courseName.length(); i++) {
+            courses.add(courseName);
+        }
+        return courses;
+    }
+
+    /**
+     * Used to access the list of courses
+     * Displays the list of courses with the number before it for each line
+     */
+    public void getCourses() {
+        for (int i = 1; i < courses.size(); i++) {
+            int courseIndex = i++;
+            System.out.println(courseIndex + ". " + courses.get(i) + "\n");
         }
     }
 
-    public void getQuizzes() throws FileNotFoundException, IOException {
+    /**
+     * Similar to getCourses, it reqtrieves the quizzes for the student
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public ArrayList<String> getQuizzes() throws FileNotFoundException, IOException {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<String> lines2 = new ArrayList<>();
+        ArrayList<String> quizNames = new ArrayList<>();
+        /**
+         * Converts the file to ArrayList for the quiz to be accessed
+         */
         try (BufferedReader br = new BufferedReader(new FileReader("Quizzes.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                quizzes[Integer.parseInt(line)] = line + ", ";
+                if (line.contains(courseName)) {
+                    lines2.add(line);
+                    line = line.substring(line.indexOf(' ') + 1);
+                    line = line.substring((line.indexOf(' ') + 1));
+                    line = line.substring(0, line.indexOf(' '));
+                    quizNames.add(line);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        int lastQuiz = quizzes.length + 1;
-        quizzes[lastQuiz] = "Exit";
-        quizName = JOptionPane.showInputDialog(null,
-                "Which quiz would you like to take?", "Brightspace",
-                JOptionPane.INFORMATION_MESSAGE, null,
-                quizzes, quizzes[lastQuiz]);
+        return quizNames;
 
-        String line = "";
-        for (int i = 0; i < lines2.size(); i++) {
-            if (lines2.get(i).contains(quizName) && lines2.get(i).contains(courseName)) {
-                line = lines2.get(i);
+    }
+
+    /**
+     * Retrieves the graded quizzes
+     */
+    public ArrayList<String> getGradedQuizzes() {
+        ArrayList<String> lines2 = new ArrayList<>();
+        ArrayList<String> quizNames = new ArrayList<>();
+        /**
+         * Converts the file to ArrayList for the quiz to be accessed
+         */
+        try (BufferedReader br = new BufferedReader(new FileReader("GradedQuizzes.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains(courseName)) {
+                    lines2.add(line);
+                    line = line.substring(line.indexOf(' ') + 1);
+                    String quizName = line.substring(0, line.indexOf(' '));
+                    line = line.substring(line.indexOf(' ') + 1);
+                    String quizFile = line.substring(0, line.indexOf(' '));
+                    line = line.substring(line.indexOf(' ') + 1);
+                    String username = line.substring(0, line.indexOf(' '));
+                    line = line.substring(line.indexOf(' ') + 1);
+                    String answers = line;
+                    String finalString = quizName + " " + username + " " + quizFile +" " + answers;
+                    quizNames.add(finalString);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if (quizName != quizzes[lastQuiz]) {
-            line = line.substring(line.indexOf(' ') + 1);
-            String quizFile = line.substring(0, line.indexOf(' '));
-            line = line.substring(line.indexOf(' ') + 1);
-            line = line.substring(line.indexOf(' ') + 1);
-            int questionNum = Integer.parseInt(line.substring(0, line.indexOf(' ')));
-            line = line.substring(line.indexOf(' ') + 1);
-            boolean bool = Boolean.parseBoolean(line);
-            Quiz quiz = new Quiz(quizFile, questionNum, quizName, bool);
-            quiz.runQuiz();
-        } else if (quizName == quizzes[lastQuiz]) {
-            JOptionPane.showConfirmDialog(null,
-            "Thank you for using Brightspace!", "Brightspace", JOptionPane.DEFAULT_OPTION);
-        }
+        return quizNames;
     }
 }
